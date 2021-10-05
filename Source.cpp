91a277622f321e142054a35208c1c521b8c93420
dominikbobrovski <91685863+dominikbobrovski@users.noreply.github.com>
2021-10-05 17:11:41 +0300

    dominik bobrovski
    sm 1

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>;
+using namespace std;
+int main()
+{
+	int R1;
+	int R2;
+	double R;
+	cout << "naiti";
+	cin >> R1 >> R2;
+	R = (R1 * R2) / (R1 + R2);
+	cout << R << endl;
+
+}
