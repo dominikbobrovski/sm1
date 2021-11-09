e2036fb4be31631afe8f00334220c8857fd59843
dominikbobrovski <91685863+dominikbobrovski@users.noreply.github.com>
2021-11-09 16:36:45 +0300

    dominik massiv

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,58 @@
-#include <iostream>;
+#include <iostream>
+#include <ctime>
 using namespace std;
-int main()
-{
-	int R1;
-	int R2;
-	double R;
-	cout << "naiti";
-	cin >> R1 >> R2;
-	R = (R1 * R2) / (R1 + R2);
-	cout << R << endl;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	/*  n 1
+	srand(time(0));
+   const int n = 100;
+   int arr[n];
+   int a = 0;
+   for(int i = 0;i < n;i++)arr[i] = rand() % 100;
+   for(int i = 0;i < n;i++) {
+   a = a + arr[i];
+   }
+   cout << a << endl;
+   */
 
+
+
+
+   /*  n 2
+	srand(time(0));
+	const int n = 80;
+	int arr[n];
+	int a = 0;
+	for(int i = 0;i < n;i++)arr[i] = rand() % 100;
+	for(int i = 0;i < n;i++) {
+   if(arr[i] > 0){
+	 a = a + arr[i];
+   }
+	}
+	cout << a << endl;
+	*/
+	/* n 3
+	 srand(time(0));
+	 const int n = 60;
+	 int arr[n];
+	 int a = 0;
+	 for(int i = 0;i < n;i++)arr[i] = rand() % 100;
+	 for(int i = 0;i < n;i++) {
+	 if(arr[i] % 3 == 0)
+	   a++;
+	 }
+	 cout << a << ' ';
+	 */
+	 /* n 4
+	srand(time(0));
+	 const int n = 90;
+	 int arr[n];
+	 int a = 1;
+	 for(int i = 0;i < n;i++)arr[i] = rand() % 100;
+	 for(int i = 0;i < n;i++) {
+	 if(arr[i] % 2 == 1)
+	   a = a * arr[i];
+	 }
+	 cout << a << ' ';
+	*/
 }
